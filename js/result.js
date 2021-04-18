ee15ee00d72ea3974f1e93fd4ffc87db63155dc2
Pavel Syunev <pavel.syunev@gmail.com>
2021-04-18 15:04:52 -0400

    Fixed css file, js files refactoring

--- js/result.js
+++ js/result.js
@@ -1,57 +1,73 @@
 $(document).ready(function () {
-  /**
-   * Gets an object and sets its content into the result card in the result page
-   * If there's no content in the JSON object, makes sure to tell the user
-   */
-  if (window.localStorage) {
-    if (localStorage.userObject) {
-      var user_object = localStorage.getItem("userObject");
-      var testVar;
-      retreivedObject = JSON.parse(user_object); //parses the retrieved object into an JSON object
-      if (JSON.stringify(retreivedObject) == "[]") {
-        $("#result-count").text("0 Results");
-        $(".result-desc").text("Try starting a new search below");
-      } else {
-        $("#result-count").text("1 Result");
-        $("#result-subtext").html(
-          "Look at the result below to see the details of the person you’re searched for."
-        );
-        $(".name").append(
-          retreivedObject.first_name + " " + retreivedObject.last_name
-        );
-        $(".user-description").append(retreivedObject.description);
-        $("#address").append("<p>" + retreivedObject.address + "</p>");
-        $(".res-email").append("<p>" + retreivedObject.email + "</p>");
+  // -------------------------------------------------- //
+  // ------------------ NEW CODE ---------------------- //
+  // -------------------------------------------------- //
 
-        for (const phone_number in retreivedObject.phone_numbers) {
-          phone = retreivedObject.phone_numbers[phone_number];
-          formatted_phone =
-            "(" +
-            phone.substring(0, 3) +
-            ") " +
-            phone.substring(3, 6) +
-            "-" +
-            phone.substring(6, 10);
+  // Show result function
+  const showResult = (retreivedObject) => {
+    const resultsNumber = window.localStorage.length;
 
-          $(".phone-num").append(
-            "<a href=" +
-              `tel:${phone}` +
-              " style='display: block;color: #004A80;'>" +
-              `${formatted_phone}` +
-              "</a>"
-          );
-        }
+    const {
+      first_name,
+      last_name,
+      description,
+      address,
+      email,
+      phone_numbers,
+    } = retreivedObject;
 
-        for (const relative in retreivedObject.relatives) {
-          $(".relatives").append(
-            "<p style='margin-bottom: 0'>" +
-              `${retreivedObject.relatives[relative]}` +
-              "</p>"
-          );
-        }
+    $("#result-count").text(
+      `${resultsNumber} ${resultsNumber <= 1 ? `Result` : `Results`}`
+    );
+    $("#result-subtext").html(
+      "Look at the result below to see the details of the person you’re searched for."
+    );
+    $(".name").append(first_name + " " + last_name);
+    $(".user-description").append(description);
+    $("#address").append("<p>" + address + "</p>");
+    $(".res-email").append("<p>" + email + "</p>");
 
-        $(".result-wrap").show();
-      }
+    for (const phone_number in phone_numbers) {
+      phone = retreivedObject.phone_numbers[phone_number];
+      formatted_phone =
+        "(" +
+        phone.substring(0, 3) +
+        ") " +
+        phone.substring(3, 6) +
+        "-" +
+        phone.substring(6, 10);
+
+      $(".phone-num").append(
+        "<a href=" +
+          `tel:${phone}` +
+          " style='display: block;color: #004A80;'>" +
+          `${formatted_phone}` +
+          "</a>"
+      );
     }
-  }
+
+    for (const relative in retreivedObject.relatives) {
+      $(".relatives").append(
+        "<p style='margin-bottom: 0'>" +
+          `${retreivedObject.relatives[relative]}` +
+          "</p>"
+      );
+    }
+
+    //If we have data to show
+    $(".result-wrap").show();
+  };
+
+  // Parce data from local storage and make main condition rendering
+  const setResultData = () => {
+    const retreivedObject = JSON.parse(localStorage.getItem("userObject"));
+
+    retreivedObject.length !== 0
+      ? showResult(retreivedObject)
+      : ($("#result-count").text("0 Results"),
+        $(".result-desc").text("Try starting a new search below"));
+  };
+
+  // If we have local storage and data we call 'setResult' func
+  window.localStorage && localStorage.userObject ? setResultData() : null;
 });
